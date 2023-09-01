bbcf8a135382c5e280ab547f7534b7dbf7f37b0a
dongmyeong yoo <boyinjeju@gmail.com>
2023-09-01 16:30:19 +0900

    #5 Global Exception Handler 추가
    Global Exception Handler 추가

--- src/main/java/whoami/boyinjeju/example/base/controller/DefaultRestController.java
+++ src/main/java/whoami/boyinjeju/example/base/controller/DefaultRestController.java
@@ -1,20 +1,29 @@
 package whoami.boyinjeju.example.base.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import whoami.boyinjeju.example.base.service.DefaultService;
+
 @RestController
-@RequestMapping("/")
-public class BaseRestController {
+@RequestMapping(value={"/", ""})
+public class DefaultRestController {
+	
+	@Autowired
+	private DefaultService defaultService;
 	
 	@GetMapping
 	public ResponseEntity<HttpStatus> home() {
 		return ResponseEntity.ok().build();
 	}
 	
-	
+	@GetMapping("/error-test")
+	public ResponseEntity<Integer> errorTest() {
+		return ResponseEntity.ok(defaultService.errorTest(100, 0));
+	}
 
 }
